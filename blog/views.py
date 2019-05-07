6219fceebeb3f4f9e13ac6ccfc4ccc40e6c9a472
bibibic <star054321@naver.com>
2019-05-07 17:47:24 +0900

    post_detail

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from . models import Post
+from django.views.generic import ListView, DetailView
+class PostList(ListView):
+    model = Post
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+
+class PostDetail(DetailView):
+    model=Post
+
+# def index(request):
+#     posts=Post.objects.all()
+#     return render(
+#         request,
+#         'blog/index.html',
+#     {'posts':posts, }
+#     )
