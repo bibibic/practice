45c869051954ee636069f68748ff3bcc3b55c48e
bibibic <star054321@naver.com>
2019-05-10 21:21:47 +0900

    html partition

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
 # Create your tests here.
+class TestView(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 2001)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        title = soup.title
+
+        print(title)
+
+
+
